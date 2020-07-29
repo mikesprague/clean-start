830429017c479a61b9ed209f72abcdeff0b38722
Michael Sprague <mikesprague@gmail.com>
2020-07-29 18:31:04 -0400

    :sparkles: Add hourly data and improve design

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import dompurify from 'dompurify';
 import he from 'he';
 import React, { useEffect, useState } from 'react';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import { apiUrl, getWeatherIcon } from '../modules/helpers';
+import { apiUrl, getWeatherIcon, initTooltips } from '../modules/helpers';
 import './Weather.scss';
 
 const Weather = (props) => {
   const [coordinates, setCoordinates] = useState(null);
   useEffect(() => {
     async function getPosition(position) {
-      console.log('position: ', position);
       setCoordinates({
         lat: position.coords.latitude,
         lng: position.coords.longitude,
@@ -40,13 +39,12 @@ const Weather = (props) => {
       async function getWeatherData (lat, lng) {
         const weatherApiurl = `${apiUrl()}/location-and-weather/?lat=${lat}&lng=${lng}`;
         const weatherData =  await axios.get(weatherApiurl)
-        .then((response) => {
-          // console.log(response.data);
-          return response.data;
-        });
-        console.log(weatherData);
+          .then((response) => {
+            // console.log(response.data);
+            return response.data;
+          });
         setData(weatherData);
-        return weatherData;
+        // return weatherData;
       };
       getWeatherData(lat, lng);
   }
@@ -54,32 +52,48 @@ const Weather = (props) => {
     return () => {};
   }, [coordinates]);
 
+  const [hourlyData, setHourlyData] = useState(null);
   useEffect(() => {
     if (data) {
-      // const weatherIcon = document.querySelector('.weather-icon');
-      // const newIconClass = getWeatherIcon(data.weather.currently.icon);
-      // console.log(weatherIcon, newIconClass);
-      // weatherIcon.classList.remove('fa-hourglass-half').add(`newIconClass`);
+      const hourly = [];
+      data.weather.hourly.data.map((hourData, index) => {
+        if (index < 5) {
+          hourly.push(hourData);
+        }
+      });
+      setHourlyData(hourly);
     }
   }, [data]);
 
+  initTooltips();
+
   return (
-    <div className="text-right weather-container">
-      <span className="weather-tooltip">
-        <span className="icon-and-temp">
-          <FontAwesomeIcon icon={data ? getWeatherIcon(data.weather.currently.icon) : 'hourglass-half' } fixedWidth className="weather-icon" />
-          <strong className="weather-temp"> {data ? Math.round(data.weather.currently.temperature) + '' + String.fromCharCode(176) : ' -- '}</strong>
-        </span>
-        <br />
-        <span className="h5 weather-location">{data ? data.location.locationName : ' Waiting for Data '}</span>
-        <br />
-        <small className="powered-by">
-          <a href="https://darksky.net/poweredby/" target="_blank" rel="noopener">
-            Powered by <FontAwesomeIcon icon="tint" fixedWidth /> Dark Sky
-          </a>
-        </small>
-      </span>
+    <div className="weather-container">
+      <h4 className="weather-location">{data ? data.location.locationName : ' Waiting for Data '}</h4>
+      <div className="icon-and-temp">
+        <FontAwesomeIcon icon={data ? getWeatherIcon(data.weather.currently.icon) : 'hourglass-half' } fixedWidth className="weather-icon" />
+        <strong className="weather-temp"> {data ? Math.round(data.weather.currently.temperature) + '' + String.fromCharCode(176) : ' -- '}</strong>
+      </div>
+      <div className={data ? 'weather-summary' : 'weather-summary hidden'}>
+        {data ? data.weather.currently.summary : ''}
+        {data && Math.round(data.weather.currently.temperature) !== Math.round(data.weather.currently.apparentTemperature) ? ' (feels ' + Math.round(data.weather.currently.apparentTemperature) + '' + String.fromCharCode(176) + ')' : ''}
+      </div>
+      <ul className="flex hourly-forecast">
+      {data && hourlyData && hourlyData.map((hour, index) => (
+        <li className="w-1/5" key={hour.time} data-tippy-content={hour ? hour.summary : ''}>
+          {dayjs.unix(hour.time).format('ha')}<br />
+          <FontAwesomeIcon icon={getWeatherIcon(hour.icon)} fixedWidth /><br />
+          {Math.round(hour.temperature)}&deg;
+        </li>
+      ))}
+      </ul>
+      <div className={data && hourlyData ? 'powered-by' : 'powered-by hidden'}>
+        <a href="https://darksky.net/poweredby/" target="_blank" rel="noopener">
+          Powered by <FontAwesomeIcon icon="tint" fixedWidth /> Dark Sky
+        </a>
+      </div>
     </div>
+
   );
 };
 
