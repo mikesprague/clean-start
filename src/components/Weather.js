89f6d53bccfeb4b941a1bed936295a29b9586715
Michael Sprague <mikesprague@gmail.com>
2020-08-03 12:20:50 -0400

    :sparkles: Add loading indicator

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -34,11 +34,13 @@ const Weather = (props) => {
     // return () => {};
   }, []);
 
+  const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState(null);
   useEffect(() => {
+    setIsLoading(true);
     if (coordinates) {
       const { lat, lng } = coordinates;
-      async function getWeatherData (lat, lng) {
+      const getWeatherData = async (lat, lng) => {
         const weatherApiurl = `${apiUrl()}/location-and-weather/?lat=${lat}&lng=${lng}`;
         const weatherData =  await axios.get(weatherApiurl)
           .then((response) => {
@@ -46,6 +48,7 @@ const Weather = (props) => {
             return response.data;
           });
         setData(weatherData);
+        setIsLoading(false);
         // return weatherData;
       };
       getWeatherData(lat, lng);
@@ -70,40 +73,42 @@ const Weather = (props) => {
 
   return (
     <div className="weather-container">
-      <h4 className="weather-location">{data ? data.location.locationName : '... Loading weather data ...'}</h4>
-      <div className={data ? 'icon-and-temp' : 'icon-and-temp hidden'}>
-        <Tippy content={data && data.weather ? data.weather.currently.summary : ''} placement="left">
-          <span>
-            <FontAwesomeIcon icon={data ? getWeatherIcon(data.weather.currently.icon) : 'hourglass-half' } fixedWidth className="weather-icon" />
-            <strong className="weather-temp">{data ? Math.round(data.weather.currently.temperature) + '' + String.fromCharCode(176) : ' -- '}</strong>
-          </span>
-        </Tippy>
-        <div className="feels-like-temp">
-          {/* Math.round(data.weather.currently.temperature) !== Math.round(data.weather.currently.apparentTemperature) */}
-          {data ? 'Feels ' + Math.round(data.weather.currently.apparentTemperature) + '' + String.fromCharCode(176) : ''}
+      <FontAwesomeIcon icon="hourglass-half" size="3x" fixedWidth spin className={isLoading ? 'block float-right mt-8' : 'hidden'} />
+      <span className={isLoading ? 'invisible' : 'visible'}>
+        <h4 className="weather-location">{data ? data.location.locationName : ''}</h4>
+        <div className="icon-and-temp">
+          <Tippy content={data && data.weather ? data.weather.currently.summary : ''} placement="left">
+            <span>
+              <FontAwesomeIcon icon={data ? getWeatherIcon(data.weather.currently.icon) : 'hourglass-half' } fixedWidth className="weather-icon" />
+              <strong className="weather-temp">{data ? Math.round(data.weather.currently.temperature) + '' + String.fromCharCode(176) : ' -- '}</strong>
+            </span>
+          </Tippy>
+          <div className="feels-like-temp">
+            {/* Math.round(data.weather.currently.temperature) !== Math.round(data.weather.currently.apparentTemperature) */}
+            {data ? 'Feels ' + Math.round(data.weather.currently.apparentTemperature) + '' + String.fromCharCode(176) : ''}
+          </div>
         </div>
-      </div>
-      <ul className="flex hourly-forecast">
-      {data && hourlyData && hourlyData.map((hour, index) => (
-        <Tippy
-          content={hour ? `${hour.summary} (Feels ${Math.round(hour.apparentTemperature)}${String.fromCharCode(176)})` : ''}
-          placement="left"
-        >
-          <li key={nanoid(8)} className="w-1/4">
-            {dayjs.unix(hour.time).format('ha')}<br />
-            <FontAwesomeIcon icon={getWeatherIcon(hour.icon)} fixedWidth />
-            {' ' + Math.round(hour.temperature)}&deg;
-          </li>
-        </Tippy>
-      ))}
-      </ul>
-      <div className={data && hourlyData ? 'powered-by' : 'powered-by hidden'}>
-        <a href="https://darksky.net/poweredby/" target="_blank" rel="noopener">
-          Powered by <FontAwesomeIcon icon="tint" fixedWidth /> Dark Sky
-        </a>
-      </div>
+        <ul className="flex hourly-forecast">
+        {data && hourlyData && hourlyData.map((hour, index) => (
+          <Tippy
+            content={hour ? `${hour.summary} (Feels ${Math.round(hour.apparentTemperature)}${String.fromCharCode(176)})` : ''}
+            placement="left"
+          >
+            <li key={nanoid(8)} className="w-1/4">
+              {dayjs.unix(hour.time).format('ha')}<br />
+              <FontAwesomeIcon icon={getWeatherIcon(hour.icon)} fixedWidth />
+              {' ' + Math.round(hour.temperature)}&deg;
+            </li>
+          </Tippy>
+        ))}
+        </ul>
+        <div className={data && hourlyData ? 'powered-by' : 'powered-by hidden'}>
+          <a href="https://darksky.net/poweredby/" target="_blank" rel="noopener">
+            Powered by <FontAwesomeIcon icon="tint" fixedWidth /> Dark Sky
+          </a>
+        </div>
+      </span>
     </div>
-
   );
 };
 
