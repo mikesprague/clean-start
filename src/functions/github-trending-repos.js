d6c1b27ac0396564dcf1f84c13dee8450b577b0d
Michael Sprague <mikesprague@gmail.com>
2021-03-18 11:38:12 -0400

    :alien: Refactor to account for markup/class name changes in trending page

--- src/functions/github-trending-repos.js
+++ src/functions/github-trending-repos.js
@@ -7,31 +7,56 @@ exports.handler = async (event, context, callback) => {
     'Access-Control-Allow-Headers': 'Content-Type',
   };
 
-  const postsData = await axios.get('https://github.com/trending?spoken_language_code=en')
+  const postsData = await axios
+    .get('https://github.com/trending?spoken_language_code=en')
     .then((response) => {
       const markup = response.data;
-      const rowSelector = 'body > div.application-main > main > div.position-relative.container-lg.p-responsive.pt-6 > div > div:nth-child(2) > article';
+      const rowSelector = 'article.Box-row';
       const linkTitleSelector = 'h1 > a';
       const descriptionSelector = 'p';
-      // const languageSelector = 'div.f6.text-gray.mt-2 > span.d-inline-block.ml-0.mr-3';
-      const starsSelector = 'div.f6.text-gray.mt-2 > a:nth-child(2)';
-      const forksSelector = 'div.f6.text-gray.mt-2 > a:nth-child(3)';
-      const starsTodaySelector = 'div.f6.text-gray.mt-2 > span.d-inline-block.float-sm-right';
-      const languageColorSelector = 'div.f6.text-gray.mt-2 > span.d-inline-block.ml-0.mr-3 > .repo-language-color';
-      const languageNameSelector = 'div.f6.text-gray.mt-2 > span.d-inline-block.ml-0.mr-3 > span:nth-child(2)';
+      const languageSelector = 'div.f6.color-text-secondary.mt-2';
+      const starsSelector = `${languageSelector} > a:nth-child(2)`;
+      const forksSelector = `${languageSelector} > a:nth-child(3)`;
+      const starsTodaySelector = `${languageSelector} > span.d-inline-block.float-sm-right`;
+      const languageColorSelector = `${languageSelector} > span.d-inline-block.ml-0.mr-3 > .repo-language-color`;
+      const languageNameSelector = `${languageSelector} > span.d-inline-block.ml-0.mr-3 > span:nth-child(2)`;
       const $ = cheerio.load(markup);
       const returnData = [];
       $(rowSelector).each((i, elem) => {
         const title = $(elem).find(linkTitleSelector).attr('href').substring(1);
         const link = `https://github.com${$(elem).find(linkTitleSelector).attr('href')}`;
-        const description = $(elem).find(descriptionSelector).text().replace(/\r?\n|\r/, '').trim();
+        const description = $(elem)
+          .find(descriptionSelector)
+          .text()
+          .replace(/\r?\n|\r/, '')
+          .trim();
         const forksLink = `https://github.com${$(elem).find(forksSelector).attr('href')}`;
         const starsLink = `https://github.com${$(elem).find(starsSelector).attr('href')}`;
-        const stars = $(elem).find(starsSelector).text().replace(/\r?\n|\r/, '').trim();
-        const forks = $(elem).find(forksSelector).text().replace(/\r?\n|\r/, '').trim();
-        const starsToday = $(elem).find(starsTodaySelector).text().replace(/\r?\n|\r/, '').trim();
-        const languageStyle = $(elem).find(languageColorSelector).attr('style') ? $(elem).find(languageColorSelector).attr('style') : null;
-        const languageName = languageStyle ? $(elem).find(languageNameSelector).text().replace(/\r?\n|\r/, '').trim() : null;
+        const stars = $(elem)
+          .find(starsSelector)
+          .text()
+          .replace(/\r?\n|\r/, '')
+          .trim();
+        const forks = $(elem)
+          .find(forksSelector)
+          .text()
+          .replace(/\r?\n|\r/, '')
+          .trim();
+        const starsToday = $(elem)
+          .find(starsTodaySelector)
+          .text()
+          .replace(/\r?\n|\r/, '')
+          .trim();
+        const languageStyle = $(elem).find(languageColorSelector).attr('style')
+          ? $(elem).find(languageColorSelector).attr('style')
+          : null;
+        const languageName = languageStyle
+          ? $(elem)
+              .find(languageNameSelector)
+              .text()
+              .replace(/\r?\n|\r/, '')
+              .trim()
+          : null;
 
         returnData.push({
           title,
@@ -47,7 +72,8 @@ exports.handler = async (event, context, callback) => {
         });
       });
       return returnData;
-    }).catch((error) => {
+    })
+    .catch((error) => {
       console.error(error);
       return {
         headers: callbackHeaders,
