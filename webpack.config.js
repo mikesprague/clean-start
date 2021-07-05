88ff7dc718ff3adadf2fa5c36f115c30072c44aa
Michael Sprague <mikesprague@gmail.com>
2021-07-05 14:58:32 -0400

    :wrench: Exclude chrom-extension URLs in workbox config

--- webpack.config.js
+++ webpack.config.js
@@ -93,6 +93,7 @@ const webpackPlugins = [
   new WorkboxPlugin.GenerateSW({
     cleanupOutdatedCaches: true,
     clientsClaim: true,
+    exclude: [/chrome-extension:\/\//],
     skipWaiting: true,
   }),
   new CompressionPlugin(),
