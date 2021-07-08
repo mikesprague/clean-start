e42163921fc22de2c4b8eb6fdab74aa8b2f13f1f
Michael Sprague <mikesprague@gmail.com>
2021-07-08 18:48:37 -0400

    :speech_balloon: Add version to name
    Also fixes linter warnings

--- src/components/TitleAndLinks.js
+++ src/components/TitleAndLinks.js
@@ -1,9 +1,10 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Tippy from '@tippyjs/react';
+import { version } from '../../package.json';
 import './TitleAndLinks.scss';
 
-const TitleAndLinks = (props) => {
+const TitleAndLinks = () => {
   const [isExtension, setIsExtension] = useState(true);
 
   useEffect(() => {
@@ -15,8 +16,8 @@ const TitleAndLinks = (props) => {
   const installLinks = (
     <h2 className="install-links">
       <Tippy content="Install via Chrome Store" placement="left">
-        <a href="https://chrome.google.com/webstore/detail/mmnlbcjgkfloemcbbjhklbblhbcjhmol" target="_blank" rel="noopener">
-          <FontAwesomeIcon icon={["fab", "chrome"]} fixedWidth />
+        <a href="https://chrome.google.com/webstore/detail/mmnlbcjgkfloemcbbjhklbblhbcjhmol" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={['fab', 'chrome']} fixedWidth />
         </a>
       </Tippy>
     </h2>
@@ -24,13 +25,13 @@ const TitleAndLinks = (props) => {
 
   return (
     <section>
-      <h1 className="app-name"><span className="pwa-link-tooltip">Clean Start</span></h1>
+      <h1 className="app-name"><span className="pwa-link-tooltip">Clean Start</span><small className="ml-2 text-xs font-extralight">v{version}</small></h1>
       <Tippy content="Source code available on GitHub" placement="left">
-        <a className="source-link" href="https://github.com/mikesprague/clean-start" target="_blank" rel="noopener">Open Source New Tab Extension</a>
+        <a className="source-link" href="https://github.com/mikesprague/clean-start" target="_blank" rel="noopener noreferrer">Open Source New Tab Extension</a>
       </Tippy>
       {isExtension ? '' : installLinks}
     </section>
-  )
+  );
 };
 
 export default TitleAndLinks;
