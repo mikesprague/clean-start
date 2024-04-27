a6a4fa9a1eaf56ac0943cc44cb3e6dbf04d5e373
Michael Sprague <mikesprague@gmail.com>
2024-04-27 09:32:59 -0400

    refactor(api/location-and-weather): update URL

--- functions/api/location-and-weather.js
+++ functions/api/location-and-weather.js
@@ -23,10 +23,10 @@ export const onRequestGet = async (context) => {
   const healthcheck = urlParams.get('healthcheck');
   let lat = urlParams.get('lat') || cf.latitude;
   let lng = urlParams.get('lng') || cf.longitude;
-  const units = urlParams.get('units') || 'imperial';
+  // const units = urlParams.get('units') || 'imperial';
 
-  lat = parseFloat(lat).toFixed(5).toString();
-  lng = parseFloat(lng).toFixed(5).toString();
+  lat = Number.parseFloat(lat).toFixed(5).toString();
+  lng = Number.parseFloat(lng).toFixed(5).toString();
 
   const { city, country, region, regionCode } = cf;
 
@@ -39,7 +39,7 @@ export const onRequestGet = async (context) => {
 
   const { OPEN_WEATHERMAP_API_KEY } = context.env;
 
-  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=${units}&appid=${OPEN_WEATHERMAP_API_KEY}`;
+  const weatherApiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lng}&appid=${OPEN_WEATHERMAP_API_KEY}`;
 
   const locationData = {
     location: {
